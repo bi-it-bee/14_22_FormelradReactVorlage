dd8bbafef105a61fb0ed337f62ecd1c1d632086b
bi-it-bee <emmanuil.bernadin@espas.ch>
2026-06-09 13:59:38 +0200

    featureMessage

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -8,7 +8,8 @@ export default function Formelrad() {
         u: 10,
         i: 2,
         r: "",
-        p: ""
+        p: "",
+        message: ""
     })
 
     const handleSubmit = (event) => {
@@ -26,11 +27,7 @@ export default function Formelrad() {
             /*calculate u and p */
             setValues(values => ({...values, u: values.i * values.r}));
             setValues(values => ({...values, p: values.i * values.i * values.r}));
-        }
-    const calculate = (event) => {
-        event.preventDefault();
-        console.log("calculate");
-        if (values.i === "" && values.r === "") {
+        } else if (values.i === "" && values.r === "") {
             /*calculate i and r */
             setValues(values => ({...values, i: values.p / values.u}));
             setValues(values => ({...values, r: values.u * values.u / values.p}));
@@ -52,19 +49,15 @@ export default function Formelrad() {
                     <h2>Formelrad</h2>
                     <img src={formelrad} width="200" alt="Formelrad"/>
                 </header>
-
                 <form onSubmit={handleSubmit}>
-                    
-                </form>
-                <form onSubmit={calculate}>
-
                     <InputField color={"black"} value={values.u} label="Spannung" handleChange={e => {setValues(values => ({...values, u: e.target.value}))}} />
                     <InputField color={"black"} value={values.i} label="Stromstärke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}} />
                     <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
                     <InputField color={"black"} value={values.p} label="Leistung" handleChange={e => {setValues(values => ({...values, p: e.target.value}))}} />
                     <button type="submit">Calculate</button>
+                    <p>{values.message}</p>
                 </form>
             </section>
         </>
     )
-}}
+}
